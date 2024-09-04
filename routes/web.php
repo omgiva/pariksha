eedf86e68130e84b0c1a16415110fe4bc08def6d
Shyam S <shyam@frontiernxt.com>
2024-09-04 21:36:48 +0100

    Completed web views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ArticleController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles/bootstrap', [ArticleController::class, 'indexBootstrap']);
+
+Route::get('/articles/tailwindcss', [ArticleController::class, 'indexTailwind']);
